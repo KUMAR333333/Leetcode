fa9dd3764252020f0457972eafdd6be1dd3433e9
Nithish <99970091+KUMAR333333@users.noreply.github.com>
2025-02-18 09:36:27 +0530

    Day24.py

--- Day24.py
+++ Day24.py
@@ -3,12 +3,11 @@ class Solution:
         if n < 2:
             return 0
         
-        prime = [0] * n  # Array to track prime numbers
+        prime = [0] * n  
         
         for i in range(2, int(n ** 0.5) + 1):  
             if prime[i] == 0:  # If i is prime
                 for j in range(i * i, n, i):
-                    prime[j] = 1  # Mark multiples of i as non-prime
+                    prime[j] = 1 
         
-        # Count numbers that are still marked as prime
         return sum(1 for i in range(2, n) if prime[i] == 0)
