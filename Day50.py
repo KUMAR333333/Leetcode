0be8919013b50e687488171aeeac54164baca79b
Nithish <99970091+KUMAR333333@users.noreply.github.com>
2025-03-16 21:02:30 +0530

    Day50.py

---
+++ Day50.py
@@ -0,0 +1,18 @@
+class Solution:
+    def tot_cars(self, minimum, ranks):
+        cnt = 0
+        for rank in ranks:
+            cnt += math.isqrt(minimum // rank)
+        return cnt
+
+    def repairCars(self, ranks: List[int], cars: int) -> int:
+        low, high = 1, max(ranks) * cars * cars
+
+        while low <= high:
+            mid = (low + high) // 2
+            if self.tot_cars(mid, ranks) >= cars:
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return low
