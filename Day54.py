fb7ccb99591ef829e27590141fee8fb3f10ed920
Nithish <99970091+KUMAR333333@users.noreply.github.com>
2025-03-20 22:20:40 +0530

    Day54.py

---
+++ Day54.py
@@ -0,0 +1,11 @@
+def sum_of_primes(n):
+    total = 0
+    for num in range(2, n + 1):
+        is_prime = True
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
+            total += num
+    return total
